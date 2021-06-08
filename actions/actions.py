9804445f12e1cfce4ef1e98d122de49424f4a504
GudaDharani <67778272+GudaDharani@users.noreply.github.com>
2021-06-08 20:21:12 +0530

    PHASE 1

--- actions/actions.py
+++ actions/actions.py
@@ -12,7 +12,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
-from .cuisine_restaurants import Restaurant_List
+from .cuisine_restaurants import check_location,check_cuisine,find_cuisine,restaurant_search
 
 class ActionCustomCuisine(Action):
 
@@ -24,15 +24,31 @@ class ActionCustomCuisine(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
         print(tracker.get_slot("cuisine"))
+        print(tracker.get_slot("location"))
         print(tracker.get_intent_of_latest_message())
-        entity=tracker.get_slot("cuisine")
-        if entity not in Restaurant_List.keys():
-            dispatcher.utter_message(response="utter_sorry_cuisine")
+        entity_cuisnie=tracker.get_slot("cuisine")
+        loc=tracker.get_slot("location")
+        if(loc==None):
+            return [SlotSet("restaurants",None)]
+        entity_id,entity_type=check_location(loc)
+        print(entity_id,entity_type)
+        if entity_id==None and entity_type==None:
             return [SlotSet("cuisine",None)]
         else:
-            restaurants=Restaurant_List[entity]
-            dispatcher.utter_message(text = f"Let me find some restaurants for {entity} cuisine for you")
-            return [SlotSet("restaurants",restaurants)]
+            if entity_cuisnie==None:
+                entity_cuisnie=find_cuisine(entity_id,entity_type)
+                dispatcher.utter_message(text=f"{entity_cuisnie} is the top cusinie at your area.I will find restaurants according to it.")
+                SlotSet("cuisine",entity_cuisnie)
+                restaurants=restaurant_search(entity_id,entity_type,entity_cuisnie)
+                return [SlotSet("restaurants",restaurants)]
+            else:
+                cuisine_id=check_cuisine(entity_id,entity_cuisnie)
+                if cuisine_id ==None:
+                    return [SlotSet("cuisine",None)]
+                else:
+                    restaurants=restaurant_search(entity_id,entity_type,entity_cuisnie)
+                    dispatcher.utter_message(text = f"Let me find some restaurants for {entity_cuisnie} cuisine for you in {loc}.")
+                    return [SlotSet("restaurants",restaurants)]
 
 
 
@@ -45,6 +61,10 @@ class ActionFindRestaurants(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         restaurants= tracker.get_slot("restaurants")
+        if restaurants is None:
+            dispatcher.utter_message(text="Sorry i cannot find the result for the preferences you mentioned.")
+            dispatcher.utter_message(text="May be you can check zomato website for further information")
+            return [SlotSet("cuisine",None)]
         restaurants= ",".join(i for i in restaurants)
         dispatcher.utter_message(text = f"These are some restaurants I found {restaurants}")
 
--- actions/cuisine_restaurants.py
+++ actions/cuisine_restaurants.py
@@ -1,1 +1,75 @@
-Restaurant_List={"indian":["urba Tadka","Goilas Butter chicken","Shiv sagar"]}
+import requests
+import random
+
+def check_location(loc):
+    loc=loc[0].upper()+loc[1:].lower()
+    url = "https://developers.zomato.com/api/v2.1/locations?query="+loc
+    payload={}
+    headers = {
+    'user-key': 'dc4f13f34b2754f1b044d01e36008001',
+    'Cookie': 'zl=en; fbtrack=4ab6df8721591043ea5d457ed1b3a266; AWSALBTG=G5FkFTw+4mX6npjwqrbdya76NwGh1xjt59uV2V+LuvXUcmUmEBNxyPlqi1OFgxxi4K4Cr2FZ5fjYvmVNHr20HadMUZZ1MdZUcf6Gnk+ICSGQvoHCfYlYmYiFUb5+7kfImu7TeleV1x4pP3JjEDMeC6+Zb00TOyE72FkJa2730hdTZYVq92k=; AWSALBTGCORS=G5FkFTw+4mX6npjwqrbdya76NwGh1xjt59uV2V+LuvXUcmUmEBNxyPlqi1OFgxxi4K4Cr2FZ5fjYvmVNHr20HadMUZZ1MdZUcf6Gnk+ICSGQvoHCfYlYmYiFUb5+7kfImu7TeleV1x4pP3JjEDMeC6+Zb00TOyE72FkJa2730hdTZYVq92k=; csrf=2969c9d7bf16b0eea6443d567309bc7f; fbcity=11434'
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    result=response.json()
+    if(len(result["location_suggestions"])!=0):
+        entity_id=result["location_suggestions"][0]["entity_id"]
+        entity_type=result["location_suggestions"][0]["entity_type"]
+        return entity_id,entity_type
+    else:
+        return None,None
+
+def check_cuisine(city_id,cuisine):
+    cuisine=cuisine[0].upper()+cuisine[1:].lower()
+    url = "https://developers.zomato.com/api/v2.1/cuisines?city_id="+str(city_id)
+    payload={}
+    headers = {
+    'user-key': 'dc4f13f34b2754f1b044d01e36008001',
+    'Cookie': 'zl=en; fbtrack=4ab6df8721591043ea5d457ed1b3a266; fbcity=11434; AWSALBTG=g9o9mHj+puqAN+cLUpHaLg0xILc1R2mT3NFV8SNRrp+ISk9tj5kbdmZXCIM2pikiem3jv5Usf5r6jeod2ogfr9qKdBfRsBF/egwrDpBwRafUzA/Mv2OqGPJcw36a/1jzvr2yuFUTzt6fpssCW4kgLEctp4ZM5UwFLKTdaCC1sfFXNV90WV4=; AWSALBTGCORS=g9o9mHj+puqAN+cLUpHaLg0xILc1R2mT3NFV8SNRrp+ISk9tj5kbdmZXCIM2pikiem3jv5Usf5r6jeod2ogfr9qKdBfRsBF/egwrDpBwRafUzA/Mv2OqGPJcw36a/1jzvr2yuFUTzt6fpssCW4kgLEctp4ZM5UwFLKTdaCC1sfFXNV90WV4=; csrf=804ff4a8a933213b83e9eccd71c0a3c8'
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    result=response.json()
+    _cuisine=result["cuisines"]
+    cuisine_id=[]
+    cuisine_name=[]
+    for i in _cuisine:
+        cuisine_id.append(i['cuisine']['cuisine_id'])
+        cuisine_name.append(i['cuisine']['cuisine_name'])
+    res = {cuisine_name[i]: cuisine_id[i] for i in range(len(cuisine_name))}
+    if(cuisine in cuisine_name):
+        return res[cuisine]
+    else:
+        return None
+
+def find_cuisine(entity_id,entity_type):
+    url = "https://developers.zomato.com/api/v2.1/location_details?entity_id="+str(entity_id)+"&entity_type="+entity_type
+
+    payload={}
+    headers = {
+    'user-key': 'dc4f13f34b2754f1b044d01e36008001',
+    'Cookie': 'zl=en; fbtrack=4ab6df8721591043ea5d457ed1b3a266; AWSALBTG=OrSP8j41YTfar1sXdv59MBAoQ29ChDRXysavGW8KVb25szRFOlzTAQ9w7/XK4k3EYw3iFORbs/8tHZ0UW1go2W84hkAcgdU2HOk4/48KJ7Z4BXwTyxrNQPfi5SiMnX2fIKI7ajXGLUfXmXAk1ClPYW6XwkN2B033wFMzgWtmNtGYiD7WwhM=; AWSALBTGCORS=OrSP8j41YTfar1sXdv59MBAoQ29ChDRXysavGW8KVb25szRFOlzTAQ9w7/XK4k3EYw3iFORbs/8tHZ0UW1go2W84hkAcgdU2HOk4/48KJ7Z4BXwTyxrNQPfi5SiMnX2fIKI7ajXGLUfXmXAk1ClPYW6XwkN2B033wFMzgWtmNtGYiD7WwhM=; csrf=e0772b51788e01bed87602470538b3b1; fbcity=7'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    result=response.json()
+    top_cuisine=result["top_cuisines"]
+    cuisine=random.choice(top_cuisine)
+    return cuisine
+
+def restaurant_search(entity_id,entity_type,cuisine):
+    url = "https://developers.zomato.com/api/v2.1/search?count=10&cuisine="+cuisine+"&entity_id="+str(entity_id)+"&entity_type="+entity_type+"&sort=ratings&order=asc"
+    payload={}
+    headers = {
+    'user-key': 'dc4f13f34b2754f1b044d01e36008001',
+    'Cookie': 'zl=en; fbtrack=4ab6df8721591043ea5d457ed1b3a266; fbcity=7; AWSALBTG=Hwq4nZave9ZDboNkF0dmsrS07aqijF97p7O7T4JYRJXFpVDDwklJF9coTFWfMq41jyF96iSRXewiJugCx4ntk9FWfiZc8EelPDe+gABYD/hUFeHjL05j6OKCuRnfozCNVzQDQ7vEk4PSEYupMlwKBXXHIKFMmjwYFb1P267fuGUW4EoyBck=; AWSALBTGCORS=Hwq4nZave9ZDboNkF0dmsrS07aqijF97p7O7T4JYRJXFpVDDwklJF9coTFWfMq41jyF96iSRXewiJugCx4ntk9FWfiZc8EelPDe+gABYD/hUFeHjL05j6OKCuRnfozCNVzQDQ7vEk4PSEYupMlwKBXXHIKFMmjwYFb1P267fuGUW4EoyBck=; csrf=541d465da19a3208e60d7d6b8a04dc78'
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    result=response.json()
+    restaurant=result["restaurants"]
+    restaurants=[]
+    c=0
+    for i in restaurant:
+        if(i["restaurant"]["name"] not in restaurants and c!=5):
+            restaurants.append(i["restaurant"]["name"])
+            c+=1
+    return restaurants
+
