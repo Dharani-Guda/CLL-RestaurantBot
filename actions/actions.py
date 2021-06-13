c9b3ef9015914a06a71ab2b3fd664960e775fbe9
GudaDharani <67778272+GudaDharani@users.noreply.github.com>
2021-06-13 12:32:24 +0530

    Phase 1

--- actions/actions.py
+++ actions/actions.py
@@ -13,6 +13,7 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 from .cuisine_restaurants import check_location,check_cuisine,find_cuisine,restaurant_search
+from collections.abc import Iterable
 
 class ActionCustomCuisine(Action):
 
@@ -26,28 +27,35 @@ class ActionCustomCuisine(Action):
         print(tracker.get_slot("cuisine"))
         print(tracker.get_slot("location"))
         print(tracker.get_intent_of_latest_message())
-        entity_cuisnie=tracker.get_slot("cuisine")
+        entity_cuisine=tracker.get_slot("cuisine")
         loc=tracker.get_slot("location")
-        if(loc==None):
+        print(1)
+        if not(loc):
+            print(2)
             return [SlotSet("restaurants",None)]
         entity_id,entity_type=check_location(loc)
         print(entity_id,entity_type)
-        if entity_id==None and entity_type==None:
+        if not(entity_id) and not(entity_type):
+            print(3)
             return [SlotSet("cuisine",None)]
         else:
-            if entity_cuisnie==None:
+            if not(entity_cuisine):
+                print(4)
                 entity_cuisnie=find_cuisine(entity_id,entity_type)
-                dispatcher.utter_message(text=f"{entity_cuisnie} is the top cusinie at your area.I will find restaurants according to it.")
+                dispatcher.utter_message(text=f"{entity_cuisnie} is the top cuisine at your area.I will find restaurants according to it.")
                 SlotSet("cuisine",entity_cuisnie)
                 restaurants=restaurant_search(entity_id,entity_type,entity_cuisnie)
                 return [SlotSet("restaurants",restaurants)]
             else:
-                cuisine_id=check_cuisine(entity_id,entity_cuisnie)
-                if cuisine_id ==None:
+                print(5)
+                cuisine_id=check_cuisine(entity_id,entity_cuisine)
+                if not(cuisine_id):
+                    print(6)
                     return [SlotSet("cuisine",None)]
                 else:
-                    restaurants=restaurant_search(entity_id,entity_type,entity_cuisnie)
-                    dispatcher.utter_message(text = f"Let me find some restaurants for {entity_cuisnie} cuisine for you in {loc}.")
+                    print(7)
+                    restaurants=restaurant_search(entity_id,entity_type,entity_cuisine)
+                    dispatcher.utter_message(text = f"Let me find some restaurants for {entity_cuisine} cuisine for you in {loc}.")
                     return [SlotSet("restaurants",restaurants)]
 
 
@@ -61,10 +69,13 @@ class ActionFindRestaurants(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         restaurants= tracker.get_slot("restaurants")
-        if restaurants is None:
-            dispatcher.utter_message(text="Sorry i cannot find the result for the preferences you mentioned.")
-            dispatcher.utter_message(text="May be you can check zomato website for further information")
+        print(restaurants)
+        print(8)
+        if not(restaurants) or not(isinstance(restaurants, Iterable)) :
+            print(9)
+            dispatcher.utter_message(response="utter_dont_know_location")
             return [SlotSet("cuisine",None)]
+        print(10)
         restaurants= ",".join(i for i in restaurants)
         dispatcher.utter_message(text = f"These are some restaurants I found {restaurants}")
 
