cf761ec0e55cc26bf404f01b9c3cc27e54b59c3c
GudaDharani <67778272+GudaDharani@users.noreply.github.com>
2021-06-14 20:20:45 +0530

    TEST NLU

--- actions/actions.py
+++ actions/actions.py
@@ -29,31 +29,31 @@ class ActionCustomCuisine(Action):
         print(tracker.get_intent_of_latest_message())
         entity_cuisine=tracker.get_slot("cuisine")
         loc=tracker.get_slot("location")
-        print(1)
+        
         if not(loc):
-            print(2)
+            
             return [SlotSet("restaurants",None)]
         entity_id,entity_type=check_location(loc)
         print(entity_id,entity_type)
         if not(entity_id) and not(entity_type):
-            print(3)
+            
             return [SlotSet("cuisine",None)]
         else:
             if not(entity_cuisine):
-                print(4)
+                
                 entity_cuisnie=find_cuisine(entity_id,entity_type)
                 dispatcher.utter_message(text=f"{entity_cuisnie} is the top cuisine at your area.I will find restaurants according to it.")
                 SlotSet("cuisine",entity_cuisnie)
                 restaurants=restaurant_search(entity_id,entity_type,entity_cuisnie)
                 return [SlotSet("restaurants",restaurants)]
             else:
-                print(5)
+
                 cuisine_id=check_cuisine(entity_id,entity_cuisine)
                 if not(cuisine_id):
-                    print(6)
+                    
                     return [SlotSet("cuisine",None)]
                 else:
-                    print(7)
+                    
                     restaurants=restaurant_search(entity_id,entity_type,entity_cuisine)
                     dispatcher.utter_message(text = f"Let me find some restaurants for {entity_cuisine} cuisine for you in {loc}.")
                     return [SlotSet("restaurants",restaurants)]
@@ -70,12 +70,12 @@ class ActionFindRestaurants(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         restaurants= tracker.get_slot("restaurants")
         print(restaurants)
-        print(8)
+        
         if not(restaurants) or not(isinstance(restaurants, Iterable)) :
-            print(9)
+
             dispatcher.utter_message(response="utter_dont_know_location")
             return [SlotSet("cuisine",None)]
-        print(10)
+
         restaurants= ",".join(i for i in restaurants)
         dispatcher.utter_message(text = f"These are some restaurants I found {restaurants}")
 
